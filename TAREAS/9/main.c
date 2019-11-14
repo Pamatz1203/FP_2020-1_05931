29d3818d0f6d2d4788f1f367f646d19f9139dd6c
Pamatz1203 <54510392+Pamatz1203@users.noreply.github.com>
2019-11-13 22:29:52 -0600

    Create main.c

---
+++ TAREAS/9/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stlib.h>
+#include <string.h>
+int main(int arc, char *argu[]){
+	int nuD;
+	//nuD es el número de digitos
+	nuD=strlen(argu[1]);
+	//es un nueva lista ara guardar el numero
+	char arguc[nuD];
+	int result=0;
+	int dig[nuD];
+	for(int i=0;i<nuD;i++){
+		//se suman los caracteres con respecto al número de digitos
+		arguc[i]=argu[1][i];
+		dig[i]=atoi(&arguc[i]);
+		//SE guarda en una lista de enteros
+		result=result+dig[i];
+		//SE suma cada digito separado
+	}
+	printf("%i\n",result);
+	return 0;
+}
